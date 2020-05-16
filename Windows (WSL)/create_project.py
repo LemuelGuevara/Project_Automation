c4956ec398489c61057e044380ddc0a2c764ca94
RiderxL <Lemuelkuya@gmail.com>
2020-05-16 20:47:39 +0800

    Updated

--- Windows (WSL)/create_project.py
+++ Windows (WSL)/create_project.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-
-=======
->>>>>>> 824b07637d0d8f3d9abd2f7d4e83d4d53aa896b7
+#!/usr/bin/python3
 import os
 import sys
 import subprocess
@@ -16,6 +13,7 @@ class Automation:
         "hub create",
         "git push -u origin master"
         ]
+
     root = "/mnt/c/Users/Lemue/Desktop/Python_Projects"
     txtEditorPath = "/mnt/c/Users/Lemue/AppData/Local/Programs/Microsoft VS Code/bin/code"
     prompt = "Create Project: "
@@ -26,7 +24,7 @@ class Automation:
         self.projectName = input(self.prompt)
         return self.setProjectPath(self.projectName)
 
-    def setProjectPath(self, projectName):
+    def setProjectPath(self, project):
         """Sets the path of the project"""
         return (os.path.join(self.root, self.projectName))
         
@@ -38,12 +36,13 @@ class Automation:
 
         try:
             os.mkdir(self.setProjectPath(1))
+            subprocess.Popen([self.txtEditorPath, self.setProjectPath(1)])
+            
             return self.makeReadmeAndFile()
 
         except Exception:
             print("Project already exists.")
             return self.getProject(), self.makeProject()
-
 
     def makeReadmeAndFile(self):    
         """Initializes a git repo along with a hub and some startup files"""
@@ -52,12 +51,7 @@ class Automation:
         for command in self.gitCommands:
             os.system(command)
 
-    
 if __name__ == "__main__":
     auto = Automation()
     auto.getProject()
-<<<<<<< HEAD
     auto.makeProject()
-=======
-    auto.makeProject()
->>>>>>> 824b07637d0d8f3d9abd2f7d4e83d4d53aa896b7
